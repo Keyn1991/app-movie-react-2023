f900395e961626b78a99e7b78d3caf55cd8db01a
potap <potapchuk50@gmail.com>
2023-12-16 14:06:54 +0100

    fix error

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import userIcon from '../../Images/DALL-Logo.png';
 import css from './UserInfo.module.css'
 const UserInfo = () => {
     return (<div className = {css.User}>
 
-            <img className={css.userIcon} src="/DALL·E%202023-12-16%2013.06.07%20-%20A%20sleek%20and%20modern%20logo%20for%20a%20movie%20website.%20The%20logo%20features%20a%20stylized%20film%20reel%20and%20a%20clapperboard,%20intertwined%20in%20a%20harmonious%20design.%20The%20film%20r.png" alt="User Login"/>
+            <img className={css.userIcon} src={userIcon} alt="User Login"/>
             <h4>Dmytro</h4>
 
 
